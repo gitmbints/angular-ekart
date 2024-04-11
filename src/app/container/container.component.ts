13fea395f5a70af2e75e8115df9b162ee7f4a290
Ambinintsoa <rahmbints@gmail.com>
2024-04-11 17:47:42 +0300

    Squash commit

---
+++ src/app/container/container.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.css'],
+})
+export class ContainerComponent {
+  // name: string = 'John Doe';
+  addToCart: number = 0;
+
+  product = {
+    name: 'iPhone',
+    price: 999,
+    color: 'Matte Black',
+    discount: 8.5,
+    inStock: 6,
+    url: '/assets/images/apple-iphone-x-pictures-5.png',
+  };
+
+  getDiscountedPrice() {
+    return (
+      this.product.price - (this.product.price * this.product.discount) / 100
+    );
+  }
+
+  decrementCartValue() {
+    if (this.addToCart > 0) {
+      this.addToCart--;
+    }
+  }
+
+  incrementCartValue() {
+    if (this.addToCart < this.product.inStock) {
+      this.addToCart++;
+    }
+  }
+}
